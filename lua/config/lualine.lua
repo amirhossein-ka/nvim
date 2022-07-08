9928f4cd2cd252a3e15c8c14901d7249de4045ee
amirhossein-ka <amirhossein.karimi@gmail.com>
2022-07-08 21:33:58 +0430

    fix lualine warning

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -18,18 +18,18 @@ function M.setup()
             lualine_x = {'encoding', 'fileformat', 'filetype'},
             lualine_y = {'progress'},
             lualine_z = {'location'},
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
-                lualine_y = {},
-                lualine_z = {},
-            },
-            tabline = {},
-            extensions = {},
-        }
-})
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {},
+        extensions = {},
+    })
 
 end
 
